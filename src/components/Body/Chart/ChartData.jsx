e0dc3c43ddb70e9575fe37071e6189d3ad5d3b37
Tolu Okunjoyo <toluokunjoyo@gmail.com>
2024-05-29 12:38:13 +0100

    updates

--- src/components/Body/Chart/ChartData.jsx
+++ src/components/Body/Chart/ChartData.jsx
@@ -7,17 +7,17 @@ const ChartData = ({ avgData }) => {
       <div className="flex flex-col gap-y-1 justify-start">
         <span className="flex items-center justify-start gap-x-2">
           <p className="rounded-full bg-[#E66FD2] w-2 h-2"></p>
-          <p className="text-defaultColorText text-[0.875rem] font-bold">
+          <p className="text-defaultColorText text-[0.875rem] font-semibold lg:font-bold">
             {" "}
             Systolic
           </p>
         </span>
-        <p className="text-defaultColorText text-[0.875rem] font-bold">
+        <p className="text-defaultColorText text-[0.875rem]  font-semibold lg:font-bold">
           {avgData.avgSys}
         </p>
         <span className="flex items-center justify-start gap-x-2">
           <img src={ArrowUp} alt="ArrowUp" className="hidden lg:block" />
-          <p className="text-defaultColorText text-[0.875rem] font-semibold">
+          <p className="text-defaultColorText text-[0.875rem] font-normal lg:font-medium">
             Higher than Average
           </p>
         </span>
