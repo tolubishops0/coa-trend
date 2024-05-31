d355041966606febdfb4e547c9e5b642355fb001
Tolu Okunjoyo <toluokunjoyo@gmail.com>
2024-05-31 11:33:13 +0100

    updates

--- src/components/Body/Chart/ChartData.jsx
+++ src/components/Body/Chart/ChartData.jsx
@@ -7,17 +7,17 @@ const ChartData = ({ avgData }) => {
       <div className="flex flex-col gap-y-1 justify-start">
         <span className="flex items-center justify-start gap-x-2">
           <p className="rounded-full bg-[#E66FD2] w-2 h-2"></p>
-          <p className="text-defaultColorText text-[0.875rem] font-semibold lg:font-bold">
+          <p className="text-defaultColorText text-[0.875rem] font-bold">
             {" "}
             Systolic
           </p>
         </span>
-        <p className="text-defaultColorText text-[0.875rem] xl:text-[1.375rem] font-semibold lg:font-bold">
+        <p className="text-defaultColorText text-[0.875rem] xl:text-[1.375rem] font-bold">
           {avgData.avgSys}
         </p>
         <span className="flex items-center justify-start gap-x-2">
           <img src={ArrowUp} alt="ArrowUp" className="hidden lg:block" />
-          <p className="text-defaultColorText text-[0.7rem] lg:text-[0.875rem] font-medium">
+          <p className="text-defaultColorText text-[0.7rem] lg:text-[0.875rem] font-semibold">
             Higher than Average
           </p>
         </span>
@@ -26,16 +26,16 @@ const ChartData = ({ avgData }) => {
       <div className="flex flex-col gap-y-1 justify-start">
         <span className="flex items-center justify-start gap-x-2">
           <p className="rounded-full bg-[#8C6FE6] w-2 h-2"></p>
-          <p className="text-defaultColorText text-[0.875rem] font-semibold lg:font-bold">
+          <p className="text-defaultColorText text-[0.875rem] font-bold">
             Diastolic
           </p>
         </span>
-        <p className="text-defaultColorText text-[0.875rem] xl:text-[1.375rem] font-semibold lg:font-bold">
+        <p className="text-defaultColorText text-[0.875rem] xl:text-[1.375rem] font-bold">
           {avgData.avgDia}
         </p>
         <span className="flex items-center justify-start gap-x-2">
           <img src={ArrowDown} alt="ArrowUp" className="hidden lg:block" />
-          <p className="text-defaultColorText text-[0.7rem] lg:text-[0.875rem] font-medium">
+          <p className="text-defaultColorText text-[0.7rem] lg:text-[0.875rem] font-semibold">
             Lower than Average
           </p>
         </span>
